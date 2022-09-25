6ac1b9dcaf25ad55db04a5eed41c5edb5f328d9a
EhitEpit <skcjddlf97@gmail.com>
2022-09-25 11:39:50 +0900

    discord.py 버전 변경으로 인한 오류 해결

--- Program.py
+++ Program.py
@@ -1,3 +1,4 @@
+import asyncio
 from discord.ext import commands
 from Command import Command
 from Status import Status
@@ -9,11 +10,16 @@ class Program:
     def __init__(self):
         self.logger = Log(self.__class__.__name__)
         if platform.system() == 'Windows':
-            self.bot = commands.Bot(command_prefix='$', activity=discord.Game(name=Status.WAITING[1]))
+            self.bot = commands.Bot(command_prefix='$', activity=discord.Game(name=Status.WAITING[1]), intents=discord.Intents.all())
         else:
-            self.bot = commands.Bot(command_prefix='!', activity=discord.Game(name=Status.WAITING[1]))
-        self.bot.add_cog(Status(self.bot))
-        self.bot.add_cog(Command(self.bot))
+            self.bot = commands.Bot(command_prefix='!', activity=discord.Game(name=Status.WAITING[1]), intents=discord.Intents.all())
+        self.status = Status(self.bot)
+        self.command = Command(self.bot)
+
+    async def setup(self):
+        await self.bot.add_cog(self.status)
+        await self.bot.add_cog(self.command)
 
     def start(self, token=""):
+        asyncio.run(self.setup())
         self.bot.run(token)
